71a31056a1db16f6f9b623c43e4c4dd0b4c1641f
Super User <prabhatsuresh5@gmail.com>
2025-03-24 18:45:00 +0530

    Moved over to the Zed editor

--- prabhat/.config/nvim/init.lua
+++ prabhat/.config/nvim/init.lua
@@ -1000,15 +1000,15 @@ require("lazy").setup({
 			require("supermaven-nvim").setup({})
 		end,
 	},
-	{
-		"MeanderingProgrammer/render-markdown.nvim",
-		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-		---@module 'render-markdown'
-		---@type render.md.UserConfig
-		opts = {},
-	},
+	-- {
+	-- 	"MeanderingProgrammer/render-markdown.nvim",
+	-- 	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+	-- 	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+	-- 	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+	-- 	---@module 'render-markdown'
+	-- 	---@type render.md.UserConfig
+	-- 	opts = {},
+	-- },
 	{ "akinsho/git-conflict.nvim" },
 	{ "whonore/Coqtail" },
 	-- { "tomtomjhj/coq-lsp.nvim" },
