f817219687df037830bdd6c187d7f3e2c46d980e
Super User <prabhatsuresh5@gmail.com>
2025-04-12 15:28:07 +0530

    added VS Code config

--- prabhat/.config/nvim/init.lua
+++ prabhat/.config/nvim/init.lua
@@ -218,7 +218,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
-vim.opt.rtp:append("/home/prabhat/.opam/default/share/ocp-indent/vim")
+vim.opt.rtp:append("/home/prabhat/.opam/latest3/share/ocp-indent/vim")
+vim.opt.rtp:append("/home/prabhat/.opam/latest3/share/merlin/vim")
 
 -- [[ Configure and install plugins ]]
 --
@@ -1000,15 +1001,15 @@ require("lazy").setup({
 			require("supermaven-nvim").setup({})
 		end,
 	},
-	-- {
-	-- 	"MeanderingProgrammer/render-markdown.nvim",
-	-- 	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-	-- 	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-	-- 	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-	-- 	---@module 'render-markdown'
-	-- 	---@type render.md.UserConfig
-	-- 	opts = {},
-	-- },
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {},
+	},
 	{ "akinsho/git-conflict.nvim" },
 	{ "whonore/Coqtail" },
 	-- { "tomtomjhj/coq-lsp.nvim" },
